b32c56da576757bfd3504964e4b38bb6a78594e8
Timothy Alicie <timothy.alicie@gmail.com>
2023-12-02 14:24:47 -0500

    Add commands ATO, ATC, ATF, ATH, ATQ, ATV, ATM, and ATX.

--- ModemCore.cs
+++ ModemCore.cs
@@ -104,10 +104,10 @@ namespace RetroModemSim
         }
 
         const int PETSCII_START = 0xC1, PETSCII_END = 0xDA, PETSCII_SHIFT = 0x80;
-        bool echo, petscii;
+        const int RESULT_CODE_ALL = int.MaxValue;
+        bool echo, petscii, zap, connected, halfDuplex, hideResponses, numericResponses;
         StringBuilder cmdStrBuilder;
-        int escapeCharCount;
-        bool zap;
+        int escapeCharCount, resultCodeLimit = RESULT_CODE_ALL;
         IDTE iDTE;
         IDiagMsg iDiagMsg;
         IModem iModem;
@@ -150,13 +150,185 @@ namespace RetroModemSim
             EscapeSequenceTimer = new Timer(OnEscapeSequenceTimeout);
 
             // Install our command handlers.
-            cmdList.Add(new CommandHandler("^E[01]?$",                              CmdEcho));
             cmdList.Add(new CommandHandler("^T$",                                   CmdToneDialing));
             cmdList.Add(new CommandHandler("^P$",                                   CmdPulseDialing));
             cmdList.Add(new CommandHandler("^Z$",                                   CmdZap));
+            cmdList.Add(new CommandHandler("^O$",                                   CmdOnline));
+            cmdList.Add(new CommandHandler("^C[01]?$",                              CmdCarrier));
+            cmdList.Add(new CommandHandler("^E[01]?$",                              CmdEcho));
+            cmdList.Add(new CommandHandler("^F[01]?$",                              CmdDuplex));
+            cmdList.Add(new CommandHandler("^H[01]?$",                              CmdHangup));
+            cmdList.Add(new CommandHandler("^Q[01]?$",                              CmdQuiet));
+            cmdList.Add(new CommandHandler("^V[01]?$",                              CmdVerbal));
+            cmdList.Add(new CommandHandler("^M[012]?$",                             CmdMonitor));
+            cmdList.Add(new CommandHandler("^X[012]?$",                             CmdResultCodeSet));
             cmdList.Add(new CommandHandler("^D.*$",                                 CmdDial));
             cmdList.Add(new CommandHandler("^S(?<reg>\\d+)\\?$",                    CmdSRegQuery));
             cmdList.Add(new CommandHandler("^S(?<reg>\\d+)=(?<val>\\d+)$",          CmdSRegSet));
+        }
+
+        /*************************************************************************************************************/
+        /// <summary>
+        /// ATX, Result Code Set Selection
+        /// </summary>
+        /*************************************************************************************************************/
+        CmdResponse CmdResultCodeSet(string cmdStr, Match match)
+        {
+            char s = '2';
+
+            if (cmdStr.Length > 1)
+            {
+                s = cmdStr[1];
+            }
+
+            switch (s)
+            {
+                case '0':
+                    iDiagMsg.WriteLine("Displaying Result Codes 0-4");
+                    resultCodeLimit = 4;
+                    break;
+
+                case '1':
+                    iDiagMsg.WriteLine("Displaying Result Codes 0-5");
+                    resultCodeLimit = 5;
+                    break;
+
+                case '2':
+                    iDiagMsg.WriteLine("Displaying All Result Codes");
+                    resultCodeLimit = RESULT_CODE_ALL;
+                    break;
+            }
+
+            return CmdRsp.Ok;
+        }
+
+        /*************************************************************************************************************/
+        /// <summary>
+        /// ATM, Monitor Speaker
+        /// </summary>
+        /*************************************************************************************************************/
+        CmdResponse CmdMonitor(string cmdStr, Match match)
+        {
+            if (cmdStr.Length == 1)
+            {
+                iDiagMsg.WriteLine("Speaker On Command Mode");
+            }
+            else
+            {
+                switch (cmdStr[1])
+                {
+                    case '0':
+                        iDiagMsg.WriteLine("Speaker Off");
+                        break;
+
+                    case '1':
+                        iDiagMsg.WriteLine("Speaker On Command Mode");
+                        break;
+
+                    case '2':
+                        iDiagMsg.WriteLine("Speaker On Always");
+                        break;
+                }
+            }
+
+            return CmdRsp.Ok;
+        }
+
+        /*************************************************************************************************************/
+        /// <summary>
+        /// ATV, Verbal
+        /// </summary>
+        /*************************************************************************************************************/
+        CmdResponse CmdVerbal(string cmdStr, Match match)
+        {
+            if ((cmdStr.Length > 1) && (cmdStr[1] == '0'))
+            {
+                iDiagMsg.WriteLine("Numeric Response Codes");
+                numericResponses = true;
+            }
+            else
+            {
+                iDiagMsg.WriteLine("Verbal Response Codes");
+                numericResponses = false;
+            }
+            return CmdRsp.Ok;
+        }
+
+        /*************************************************************************************************************/
+        /// <summary>
+        /// ATQ, Quiet
+        /// </summary>
+        /*************************************************************************************************************/
+        CmdResponse CmdQuiet(string cmdStr, Match match)
+        {
+            if ((cmdStr.Length > 1) && (cmdStr[1] == '1'))
+            {
+                iDiagMsg.WriteLine("Disabling Response Codes");
+                hideResponses = true;
+            }
+            else
+            {
+                iDiagMsg.WriteLine("Enabling Response Codes");
+                hideResponses = false;
+            }
+            return CmdRsp.Ok;
+        }
+
+        /*************************************************************************************************************/
+        /// <summary>
+        /// ATF, Full/Half Duplex
+        /// </summary>
+        /*************************************************************************************************************/
+        CmdResponse CmdDuplex(string cmdStr, Match match)
+        {
+            if ((cmdStr.Length > 1) && (cmdStr[1] == '0'))
+            {
+                iDiagMsg.WriteLine("Half Duplex");
+                halfDuplex = true;
+            }
+            else
+            {
+                iDiagMsg.WriteLine("Full Duplex");
+                halfDuplex = false;
+            }
+            return CmdRsp.Ok;
+        }
+
+        /*************************************************************************************************************/
+        /// <summary>
+        /// ATC, Transmitter Carrier.
+        /// </summary>
+        /*************************************************************************************************************/
+        CmdResponse CmdCarrier(string cmdStr, Match match)
+        {
+            if ((cmdStr.Length > 1) && (cmdStr[1] == '0'))
+            {
+                iDiagMsg.WriteLine("Carrier Off");
+            }
+            else
+            {
+                iDiagMsg.WriteLine("Carrier On");
+            }
+            return CmdRsp.Ok;
+        }
+
+        /*************************************************************************************************************/
+        /// <summary>
+        /// ATH, Hangup.
+        /// </summary>
+        /*************************************************************************************************************/
+        CmdResponse CmdHangup(string cmdStr, Match match)
+        {
+            if ((cmdStr.Length > 1) && (cmdStr[1] == '1'))
+            {
+                iDiagMsg.WriteLine("Hook Off");
+            }
+            else
+            {
+                iDiagMsg.WriteLine("Hook On");
+                Hangup();
+            }
+            return CmdRsp.Ok;
         }
 
         /*************************************************************************************************************/
@@ -170,14 +342,13 @@ namespace RetroModemSim
             {
                 echo = false;
                 iDiagMsg.WriteLine("Echo Off");
-                return CmdRsp.Ok;
             }
             else
             {
                 echo = true;
                 iDiagMsg.WriteLine("Echo On");
-                return CmdRsp.Ok;
             }
+            return CmdRsp.Ok;
         }
 
         /*************************************************************************************************************/
@@ -266,10 +437,56 @@ namespace RetroModemSim
         CmdResponse CmdZap(string cmdStr, Match match)
         {
             iDiagMsg.WriteLine("Reset All Settings to Default");
-            iModem.HangUp();
+            Hangup();
             zap = true;
 
             return CmdRsp.Ok;
+        }
+
+        /*************************************************************************************************************/
+        /// <summary>
+        /// ATO, Online, returns to online data mode from command mode.
+        /// </summary>
+        /*************************************************************************************************************/
+        CmdResponse CmdOnline(string cmdStr, Match match)
+        {
+            if (connected)
+            {
+                EnterOnlineDataMode();
+                return CmdRsp.Connect;
+            }
+            else
+            {
+                return CmdRsp.Ok;
+            }
+        }
+
+        /*************************************************************************************************************/
+        /// <summary>
+        /// Attempts to hang up the phone and terminate the connection.
+        /// </summary>
+        /*************************************************************************************************************/
+        void Hangup()
+        {
+            if (connected)
+            {
+                iDiagMsg.WriteLine("Hanging Up");
+                iModem.HangUp();
+                connected = false;
+            }
+        }
+
+        /*************************************************************************************************************/
+        /// <summary>
+        /// Enters online data mode from command mode.
+        /// </summary>
+        /*************************************************************************************************************/
+        void EnterOnlineDataMode()
+        {
+            iDiagMsg.WriteLine($"Online Data Mode");
+
+            state = StateEnum.Online;
+            escapeSw.Restart();
         }
 
         /*************************************************************************************************************/
@@ -299,6 +516,7 @@ namespace RetroModemSim
             // See if the modem was able to connect to the destination.
             if (cmdRsp == CmdRsp.Connect)
             {
+                connected = true;
                 iDiagMsg.WriteLine($"Connected to \"{cmdStr}\"");
 
                 // Inform the DTE that we're now connected (the data carrier is detected).
@@ -307,10 +525,7 @@ namespace RetroModemSim
                 // Move into online mode if requested.
                 if (enterOnlineMode)
                 {
-                    iDiagMsg.WriteLine($"Online Data Mode");
-
-                    state = StateEnum.Online;
-                    escapeSw.Restart();
+                    EnterOnlineDataMode();
                 }
             }
 
@@ -400,9 +615,16 @@ namespace RetroModemSim
         /*************************************************************************************************************/
         void SendResponse(CmdResponse rsp)
         {
-            if (rsp != CmdRsp.None)
+            if ((rsp != CmdRsp.None) && (!hideResponses) && (rsp.Code <= resultCodeLimit))
             {
-                TxLine(rsp.ResponseStr);
+                if (numericResponses)
+                {
+                    TxLine(rsp.Code.ToString());
+                }
+                else
+                {
+                    TxLine(rsp.ResponseStr);
+                }
             }
         }
 
@@ -556,6 +778,13 @@ namespace RetroModemSim
                 while (escapeCharCount > 0)
                 {
                     iModem.TxByteToRemoteHost(sReg[(int)SRegEnum.EscapeCode]);
+
+                    // Echo the data back to the DTE in half-duplex mode.
+                    if (halfDuplex)
+                    {
+                        iDTE.TxByte(sReg[(int)SRegEnum.EscapeCode]);
+                    }
+
                     escapeCharCount--;
                 }
             }
@@ -608,6 +837,12 @@ namespace RetroModemSim
                 {
                     TerminateEscapeSequence();
                     iModem.TxByteToRemoteHost(dataFromDte);
+
+                    // Echo the data back to the DTE in half-duplex mode.
+                    if (halfDuplex)
+                    {
+                        iDTE.TxByte(dataFromDte);
+                    }
                 }
 
                 escapeSw.Restart();
